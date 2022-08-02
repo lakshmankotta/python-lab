353bd483b2c4b0a70c0af8876f3e11551dfd27ab
lakshmankotta <109126558+lakshmankotta@users.noreply.github.com>
2022-08-02 09:09:07 +0530

    Delete exp6.py

--- exp6.py
+++
@@ -1,8 +0,0 @@
-import cmath
-a=int(input('enter x^2 coeff : '))
-b=int(input('enter x coeff : '))
-c=int(input('enter constant : '))
-d= (b**2)-(4*a*c)
-sol1=(-b +cmath.sqrt(d))/(2*a)
-sol2=(-b -cmath.sqrt(d))/(2*a)
-print('solutions are : ',sol1,sol2)
